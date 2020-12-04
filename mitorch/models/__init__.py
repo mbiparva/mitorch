f556ab4a4674650c46053304053ee34969dc9039
Mahdi Biparva <mahdi.biparva@sri.utoronto.ca>
2020-12-04 12:00:37 -0500

    feature/modelzoo: added munet class to models; done

--- mitorch/models/__init__.py
+++ mitorch/models/__init__.py
@@ -11,8 +11,9 @@ from .Unet3D import Unet3D
 from .NestedUnet3D import NestedUnet3D
 from .Unet3DCBAM import Unet3DCBAM
 from .DAUNet3D import DAUnet3D
+from .MBUnet3D import MBUnet3D
+from .MUnet3D import MUnet3D
 
-# TODO add https://github.com/Project-MONAI/MONAI/blob/master/monai/networks/nets/ahnet.py
 # TODO add https://github.com/Project-MONAI/MONAI/blob/master/monai/networks/nets/densenet.py
 # TODO add https://github.com/Project-MONAI/MONAI/blob/master/monai/networks/nets/dynunet.py
 # TODO add https://github.com/Project-MONAI/MONAI/blob/master/monai/networks/nets/senet.py
