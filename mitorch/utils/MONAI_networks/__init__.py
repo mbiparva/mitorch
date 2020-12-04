a7c6fb29418529fc288ea1820233c16647d28c1f
Mahdi Biparva <mahdi.biparva@sri.utoronto.ca>
2020-12-04 10:30:22 -0500

    feature/modelzoo: unet added to the net utils

--- mitorch/utils/MONAI_networks/__init__.py
+++ mitorch/utils/MONAI_networks/__init__.py
@@ -9,4 +9,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .basic_unet import BasicUNet
+from .bunet import BasicUNet
+from .unet import UNet
+
---
+++ mitorch/utils/MONAI_networks/simplelayers.py
@@ -0,0 +1,65 @@
+# Copyright 2020 MONAI Consortium
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import math
+from typing import Sequence, Union, cast
+
+import torch
+import torch.nn.functional as F
+from torch import nn
+from torch.autograd import Function
+from ..MONAI_data import Enum
+
+
+class SkipMode(Enum):
+    """
+    See also: :py:class:`monai.networks.layers.SkipConnection`
+    """
+
+    CAT = "cat"
+    ADD = "add"
+    MUL = "mul"
+
+
+class SkipConnection(nn.Module):
+    """
+    Combine the forward pass input with the result from the given submodule::
+
+        --+--submodule--o--
+          |_____________|
+
+    The available modes are ``"cat"``, ``"add"``, ``"mul"``.
+    """
+
+    def __init__(self, submodule, dim: int = 1, mode: Union[str, SkipMode] = "cat") -> None:
+        """
+
+        Args:
+            submodule: the module defines the trainable branch.
+            dim: the dimension over which the tensors are concatenated.
+                Used when mode is ``"cat"``.
+            mode: ``"cat"``, ``"add"``, ``"mul"``. defaults to ``"cat"``.
+        """
+        super().__init__()
+        self.submodule = submodule
+        self.dim = dim
+        self.mode = SkipMode(mode).value
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        y = self.submodule(x)
+
+        if self.mode == "cat":
+            return torch.cat([x, y], dim=self.dim)
+        if self.mode == "add":
+            return torch.add(x, y)
+        if self.mode == "mul":
+            return torch.mul(x, y)
+        raise NotImplementedError(f"Unsupported mode {self.mode}.")
