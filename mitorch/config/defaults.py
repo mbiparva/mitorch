4bf6e7f86da4abe73cdf8993eb15a03b05303f5d
Mahdi Biparva <mahdi.biparva@sri.utoronto.ca>
2020-12-09 17:08:49 -0500

    feature/modelzoo: debugged vnet; fups

--- mitorch/config/defaults.py
+++ mitorch/config/defaults.py
@@ -211,7 +211,7 @@ _C.MODEL.ARCH = "unet3d"
 
 # Model name
 _C.MODEL.MODEL_NAME = ('Unet3D', 'NestedUnet3D', 'Unet3DCBAM', 'DAUnet3D',
-                       'MBUnet3D', 'MUnet3D', 'Densenet3D', 'Highresnet3D', 'DYNUnet3D', 'SEUnet3D', 'Vnet3D')[0]
+                       'MBUnet3D', 'MUnet3D', 'DYNUnet3D', 'Vnet3D', 'Densenet3D', 'Highresnet3D', 'SEUnet3D')[0]
 
 _C.MODEL.PROCESSING_MODE = ('2d', '3d')[1]
 
